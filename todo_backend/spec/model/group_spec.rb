042d8542d8d2d32e8423e95bb489f9f3c841758c
23vbq <42277522+23vbq@users.noreply.github.com>
2024-08-24 14:24:12 +0200

    trying to write first test, with failure

---
+++ todo_backend/spec/model/group_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Group, type: :model do
+    # Validation tests
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name) }
+    # it { is_expected.to validates_length_of(:name).is_at_most(20)}
+end

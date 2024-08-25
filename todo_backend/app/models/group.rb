06144256a96314aaeee541c69de01d404631b3fe
23vbq <42277522+23vbq@users.noreply.github.com>
2024-08-25 21:30:08 +0200

    group model test

--- todo_backend/app/models/group.rb
+++ todo_backend/app/models/group.rb
@@ -2,6 +2,6 @@ class Group < ApplicationRecord
   has_many :to_dos
 
   validates :name, presence: true
-  validates :name, uniqueness: true
+  validates :name, uniqueness: { case_sensitive: true }
   validates :name, length: {maximum: 20}
 end
--- todo_backend/spec/model/group_spec.rb
+++ todo_backend/spec/model/group_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
+    # Associations tests
+    describe 'associations' do
+        it { should have_many(:to_dos) } 
+    end
     # Validation tests
-    it { should validate_presence_of(:name) }
-    it { should validate_uniqueness_of(:name) }
-    # it { is_expected.to validates_length_of(:name).is_at_most(20)}
+    describe 'validations' do
+        it { should validate_presence_of(:name) }
+        it { should validate_uniqueness_of(:name) }
+        it { should validate_length_of(:name).is_at_most(20)}
+    end
 end

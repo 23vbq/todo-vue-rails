f055cf57bfe635851c2d23fa6df931687597b367
23vbq <42277522+23vbq@users.noreply.github.com>
2024-08-25 21:53:46 +0200

    Feature/tests init (#4)
    * added gems
    
    * rspec init
    
    * rails_helper edit
    
    * trying to write first test, with failure
    
    * group model test
    
    * to_do model test
    
    * attachment model test
    
    * removed vscode settings

--- todo_backend/app/models/group.rb
+++ todo_backend/app/models/group.rb
@@ -2,6 +2,6 @@ class Group < ApplicationRecord
   has_many :to_dos
 
   validates :name, presence: true
-  validates :name, uniqueness: { message: 'Group with %{value} name already exists'}
+  validates :name, uniqueness: { case_sensitive: true }
   validates :name, length: {maximum: 20}
 end

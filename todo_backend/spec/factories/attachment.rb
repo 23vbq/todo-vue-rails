fa8b731145fea26cfb0ca92e4bd10c0afce4b360
23vbq <42277522+23vbq@users.noreply.github.com>
2024-10-04 22:12:37 +0200

    code style fix

--- todo_backend/spec/factories/attachment.rb
+++ todo_backend/spec/factories/attachment.rb
@@ -3,6 +3,5 @@ FactoryBot.define do
         to_do
         name { Faker::Lorem.sentence(word_count: 1, random_words_to_add: 4) }
         data { Faker::String.random(length: 16..512) }
-        end
     end
 end

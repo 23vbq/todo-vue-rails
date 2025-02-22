efc79d804fecb1d832860fae17e07d5fa829343b
23vbq <42277522+23vbq@users.noreply.github.com>
2025-02-22 15:08:11 +0100

    developement setup (#13)
    * developement setup
    
    * live changes

--- todo_frontend/vite.config.js
+++ todo_frontend/vite.config.js
@@ -7,7 +7,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    port: 8000
+    port: 8000,
+    watch: {
+      usePolling: true
+    }
   },
   plugins: [
     vue(),

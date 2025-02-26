3d523e7739ab533363a9f3f2d3283c5accab6784
23vbq <42277522+23vbq@users.noreply.github.com>
2025-02-26 21:42:09 +0100

    card view using more js

--- todo_frontend/src/components/main/MainView.vue
+++ todo_frontend/src/components/main/MainView.vue
@@ -1,31 +1,62 @@
 <script setup>
 import Header from './Header.vue';
 import Card from '../todo/Card.vue';
+
+const cards = [
+    {
+        title: 'Title',
+        date: new Date(Date.parse('2025-02-25T20:48:00.000Z')),
+        status: 0,
+        description: `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras elementum leo sit amet magna fermentum tempor. Maecenas luctus augue sit amet dolor molestie, nec ullamcorper lacus viverra. Nunc ac tempus ipsum, ut consectetur tellus. Aenean lacus tortor, hendrerit vel ante sed, tempor porta mi.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras elementum leo sit amet magna fermentum tempor. Maecenas luctus augue sit amet dolor molestie, nec ullamcorper lacus viverra. Nunc ac tempus ipsum, ut consectetur tellus. Aenean lacus tortor, hendrerit vel ante sed, tempor porta mi.
+        `,
+    },
+    {
+        title: 'Title 1',
+        date: new Date(Date.parse('2025-02-28T20:48:00.000Z')),
+        status: 0,
+        description: `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras elementum leo sit amet magna fermentum tempor. Maecenas luctus augue sit amet dolor molestie, nec ullamcorper lacus viverra. Nunc ac tempus ipsum, ut consectetur tellus. Aenean lacus tortor, hendrerit vel ante sed, tempor porta mi.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras elementum leo sit amet magna fermentum tempor. Maecenas luctus augue sit amet dolor molestie, nec ullamcorper lacus viverra. Nunc ac tempus ipsum, ut consectetur tellus. Aenean lacus tortor, hendrerit vel ante sed, tempor porta mi.
+        `,
+    },
+    {
+        title: 'Title 2',
+        date: new Date(Date.parse('2025-02-25T20:48:00.000Z')),
+        status: 1,
+        description: `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras elementum leo sit amet magna fermentum tempor. Maecenas luctus augue sit amet dolor molestie, nec ullamcorper lacus viverra. Nunc ac tempus ipsum, ut consectetur tellus. Aenean lacus tortor, hendrerit vel ante sed, tempor porta mi.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras elementum leo sit amet magna fermentum tempor. Maecenas luctus augue sit amet dolor molestie, nec ullamcorper lacus viverra. Nunc ac tempus ipsum, ut consectetur tellus. Aenean lacus tortor, hendrerit vel ante sed, tempor porta mi.
+        `,
+    },
+    {
+        title: 'Title 3',
+        date: new Date(Date.parse('2025-02-25T20:48:00.000Z')),
+        status: 0,
+        description: `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras elementum leo sit amet magna fermentum tempor. Maecenas luctus augue sit amet dolor molestie, nec ullamcorper lacus viverra. Nunc ac tempus ipsum, ut consectetur tellus. Aenean lacus tortor, hendrerit vel ante sed, tempor porta mi.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras elementum leo sit amet magna fermentum tempor. Maecenas luctus augue sit amet dolor molestie, nec ullamcorper lacus viverra. Nunc ac tempus ipsum, ut consectetur tellus. Aenean lacus tortor, hendrerit vel ante sed, tempor porta mi.
+        `,
+    },
+    {
+        title: 'Title 4',
+        date: new Date(Date.parse('2025-02-25T20:48:00.000Z')),
+        status: 0,
+        description: `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras elementum leo sit amet magna fermentum tempor. Maecenas luctus augue sit amet dolor molestie, nec ullamcorper lacus viverra. Nunc ac tempus ipsum, ut consectetur tellus. Aenean lacus tortor, hendrerit vel ante sed, tempor porta mi.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras elementum leo sit amet magna fermentum tempor. Maecenas luctus augue sit amet dolor molestie, nec ullamcorper lacus viverra. Nunc ac tempus ipsum, ut consectetur tellus. Aenean lacus tortor, hendrerit vel ante sed, tempor porta mi.
+        `,
+    },
+]
 </script>
 <template>
     <div class="flex flex-col w-full h-full px-6">
         <Header group-name="Group 1"></Header>
 
         <div class="flex flex-wrap content-start gap-4 w-full flex-1 py-4">
-            <Card title="Title" date="20:48 25.02.2025">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras elementum leo sit amet magna fermentum tempor. Maecenas luctus augue sit amet dolor molestie, nec ullamcorper lacus viverra. Nunc ac tempus ipsum, ut consectetur tellus. Aenean lacus tortor, hendrerit vel ante sed, tempor porta mi.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras elementum leo sit amet magna fermentum tempor. Maecenas luctus augue sit amet dolor molestie, nec ullamcorper lacus viverra. Nunc ac tempus ipsum, ut consectetur tellus. Aenean lacus tortor, hendrerit vel ante sed, tempor porta mi.
-            </Card>
-            <Card title="Title" date="20:48 25.02.2025">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras elementum leo sit amet magna fermentum tempor. Maecenas luctus augue sit amet dolor molestie, nec ullamcorper lacus viverra. Nunc ac tempus ipsum, ut consectetur tellus. Aenean lacus tortor, hendrerit vel ante sed, tempor porta mi.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras elementum leo sit amet magna fermentum tempor. Maecenas luctus augue sit amet dolor molestie, nec ullamcorper lacus viverra. Nunc ac tempus ipsum, ut consectetur tellus. Aenean lacus tortor, hendrerit vel ante sed, tempor porta mi.
-            </Card>
-            <Card title="Title" date="20:48 25.02.2025">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras elementum leo sit amet magna fermentum tempor. Maecenas luctus augue sit amet dolor molestie, nec ullamcorper lacus viverra. Nunc ac tempus ipsum, ut consectetur tellus. Aenean lacus tortor, hendrerit vel ante sed, tempor porta mi.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras elementum leo sit amet magna fermentum tempor. Maecenas luctus augue sit amet dolor molestie, nec ullamcorper lacus viverra. Nunc ac tempus ipsum, ut consectetur tellus. Aenean lacus tortor, hendrerit vel ante sed, tempor porta mi.
-            </Card>
-            <Card title="Title" date="20:48 25.02.2025">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras elementum leo sit amet magna fermentum tempor. Maecenas luctus augue sit amet dolor molestie, nec ullamcorper lacus viverra. Nunc ac tempus ipsum, ut consectetur tellus. Aenean lacus tortor, hendrerit vel ante sed, tempor porta mi.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras elementum leo sit amet magna fermentum tempor. Maecenas luctus augue sit amet dolor molestie, nec ullamcorper lacus viverra. Nunc ac tempus ipsum, ut consectetur tellus. Aenean lacus tortor, hendrerit vel ante sed, tempor porta mi.
-            </Card>
-            <Card title="Title" date="20:48 25.02.2025">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras elementum leo sit amet magna fermentum tempor. Maecenas luctus augue sit amet dolor molestie, nec ullamcorper lacus viverra. Nunc ac tempus ipsum, ut consectetur tellus. Aenean lacus tortor, hendrerit vel ante sed, tempor porta mi.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras elementum leo sit amet magna fermentum tempor. Maecenas luctus augue sit amet dolor molestie, nec ullamcorper lacus viverra. Nunc ac tempus ipsum, ut consectetur tellus. Aenean lacus tortor, hendrerit vel ante sed, tempor porta mi.
+            <Card v-for="card in cards" :title="card.title" :date="card.date" :status="card.status">
+                {{ card.description }}
             </Card>
         </div>
     </div>

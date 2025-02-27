9cead1a926bf171debc0121781ce24277f6e23fe
23vbq <42277522+23vbq@users.noreply.github.com>
2025-02-27 22:08:48 +0100

    change selected bg

--- todo_frontend/src/components/navigation/Group.vue
+++ todo_frontend/src/components/navigation/Group.vue
@@ -19,8 +19,8 @@ defineProps({
 
 <template>
     <li
-        class="w-full mt-1 p-1 flex items-center text-sm rounded cursor-pointer transition duration-75 hover:bg-gray-200"
-        :class="active?.value ? 'text-black font-medium' : null"
+        class="w-full mt-1 p-1 flex items-center text-sm rounded cursor-pointer transition duration-75 hover:bg-gray-200 hover:text-black"
+        :class="active?.value ? 'bg-orange-300 text-white font-medium hover:bg-orange-400 hover:text-white' : null"
     >
         <div class="w-4 mr-2" :style="color ? {'color': color} : null"><IconDot></IconDot></div>
         {{ name }}

d24a32e02aee1f31151d89765574a33d5f25071d
23vbq <42277522+23vbq@users.noreply.github.com>
2025-03-01 21:44:02 +0100

    use same modal to edit, esc to close

--- todo_frontend/src/components/modals/GroupModal.vue
+++ todo_frontend/src/components/modals/GroupModal.vue
@@ -1,13 +1,29 @@
 <script setup>
 import IconAdd from '../icons/IconAdd.vue';
+
+defineProps({
+    title: {
+        type: String,
+        required: true,
+    },
+    group: {
+        type: Object,
+        required: false,
+    },
+});
 </script>
 
 <template>
-    <div class="z-20 fixed top-0 left-0 w-screen h-screen bg-black/60 flex items-center justify-center">
+    <div
+        @keydown.esc="$emit('close')"
+        tabindex="0"
+        @vue:mounted="$event.el.focus()"
+        class="z-20 fixed top-0 left-0 w-screen h-screen bg-black/60 flex items-center justify-center"
+    >
         <div class="z-30 w-128 px-4 py-3 bg-white">
             <!-- Header -->
             <div class="flex items-center px-3 pb-2 border-b border-gray-300">
-                <h1 class="text-xl font-semibold">New group</h1>
+                <h1 class="text-xl font-semibold">{{ title }}</h1>
                 <button @click="$emit('close')" class="ml-auto p-1 rounded-md cursor-pointer transition duration-75 hover:text-black hover:bg-gray-200"><IconAdd class="w-4 rotate-45"/></button>
             </div>
             <!-- Content -->
@@ -18,6 +34,7 @@ import IconAdd from '../icons/IconAdd.vue';
                         id="group-name"
                         type="text"
                         name="name"
+                        :value="group?.name"
                         class="w-full border border-gray-400 rounded-md px-2 py-1 text-sm text-gray-700 focus:outline-orange-300"
                         required
                     />
--- todo_frontend/src/components/navigation/NavBar.vue
+++ todo_frontend/src/components/navigation/NavBar.vue
@@ -41,39 +41,60 @@ const selectGroup = (toSelect) => {
     props.selectedGroup.value = toSelect;
 }
 
-let showGroupModal = ref(false);
+let groupModal = ref({
+    title: 'New group',
+    group: null,
+    isVisible: false
+});
+
+const newGroup = () => {
+    groupModal.value.title = 'New group';
+    groupModal.value.group = null;
+    groupModal.value.isVisible = true;
+}
+
+const editGroup = (group, event) => {
+    groupModal.value.title = 'Edit group';
+    groupModal.value.group = group;
+    groupModal.value.isVisible = true;
+
+    event.stopPropagation();
+}
 </script>
 
 <template>
-  <div class="flex flex-col w-3xs min-h-screen border-r-2 border-gray-400">
-    <header class="px-4 py-2 bg-orange-300 text-white text-2xl font-bold">
-      ToDo
-    </header>
+    <div class="flex flex-col w-3xs min-h-screen border-r-2 border-gray-400">
+        <header class="px-4 py-2 bg-orange-300 text-white text-2xl font-bold">
+        ToDo
+        </header>
 
-    <div class="flex flex-col flex-1 px-3 py-2">
-        <div class="flex items-center font-semibold px-2 pb-1 border-b border-gray-400">
-            <h1 class="text-gray-900">Groups</h1>
-            <button
-                @click="showGroupModal = true"
-                class="ml-auto w-3 text-gray-600 cursor-pointer transition duration-75 hover:text-black"
-            ><IconAdd/></button>
+        <div class="flex flex-col flex-1 px-3 py-2">
+            <div class="flex items-center font-semibold px-2 pb-1 border-b border-gray-400">
+                <h1 class="text-gray-900">Groups</h1>
+                <button
+                    @click="newGroup()"
+                    class="ml-auto w-3 text-gray-600 cursor-pointer transition duration-75 hover:text-black"
+                ><IconAdd/></button>
+            </div>
+            <ul>
+                <Group
+                    v-for="group in groups"
+                    :group="group"
+                    @click="selectGroup(group)"
+                    :edit-group="editGroup"
+                />
+            </ul>
+
+            <button class="flex items-center mt-auto w-full gap-3 px-3 py-2 font-semibold rounded cursor-pointer transition duration-75 hover:bg-gray-300 hover:text-black">
+                <div class="w-4"><IconSettings></IconSettings></div>
+                Settings
+            </button>
         </div>
-        <ul>
-            <Group
-                v-for="group in groups"
-                :name="group.name"
-                :color="group.color"
-                :active="group?.active"
-                @click="selectGroup(group)"
-            />
-        </ul>
-
-        <button class="flex items-center mt-auto w-full gap-3 px-3 py-2 font-semibold rounded cursor-pointer transition duration-75 hover:bg-gray-300 hover:text-black">
-            <div class="w-4"><IconSettings></IconSettings></div>
-            Settings
-        </button>
     </div>
-
-  </div>
-  <GroupModal v-if="showGroupModal" @close="showGroupModal = false"/>
+    <GroupModal
+        v-if="groupModal.isVisible"
+        @close="groupModal.isVisible = false"
+        :title="groupModal.title"
+        :group="groupModal.group"
+    />
 </template>

437e7cffcdbb64b636f339e16b84c465928ab6a8
23vbq <42277522+23vbq@users.noreply.github.com>
2025-03-09 19:48:22 +0100

    save task data

--- todo_frontend/src/components/todo/TaskModal.vue
+++ todo_frontend/src/components/todo/TaskModal.vue
@@ -2,7 +2,7 @@
 import IconAdd from '../icons/IconAdd.vue';
 import IconClock from '../icons/IconClock.vue';
 
-defineProps({
+const props = defineProps({
     task: {
         type: Object,
         required: true,
@@ -11,6 +11,14 @@ defineProps({
 
 const formatDate = (date) => {
     return `${String(date.getDate()).padStart(2, '0')}.${String(date.getMonth() + 1).padStart(2, '0')}.${date.getFullYear()} ${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}`
+}
+
+const saveTask = () => {
+    const title = document.getElementById('task-modal-title').innerHTML;
+    const description = document.getElementById('task-modal-description').innerHTML;
+
+    props.task.title = title;
+    props.task.description = description;
 }
 </script>
 
@@ -22,13 +30,14 @@ const formatDate = (date) => {
         class="z-20 fixed top-0 left-0 w-screen h-screen bg-black/60 flex items-center justify-center"
     >
         <div class="z-30 w-2/3 px-4 py-3 bg-white">
-            <form>
             <!-- Header -->
             <div class="flex items-center">
                 <h1
+                    id="task-modal-title"
                     class="text-2xl px-3 pb-1 font-semibold w-1/2 rounded-md border-b border-white
                     transition duration-75 focus:outline-0 focus:text-black focus:border-gray-900" 
                     contenteditable="true" spellcheck="false"
+                    @keyup="saveTask()"
                 >{{ task?.title }}</h1>
                 <button @click="$emit('close')" class="ml-auto p-1 rounded-md cursor-pointer transition duration-75 hover:text-black hover:bg-gray-200"><IconAdd class="w-4 rotate-45"/></button>
             </div>
@@ -40,10 +49,12 @@ const formatDate = (date) => {
             <!-- <hr class="border-b border-gray-300"> -->
             <!-- Description -->
             <div
+                id="task-modal-description"
                 class="my-2 px-3 py-2 min-h-40 text-justify border-b border-gray-400 rounded-md
                 transition duration-75 focus:outline-0 focus:text-black focus:border-gray-900"
                 contenteditable="true"
                 spellcheck="false"
+                @keyup="saveTask()"
             >
                 {{ task?.description }}
             </div>
@@ -59,7 +70,6 @@ const formatDate = (date) => {
                         class="px-3 py-1 bg-orange-300 text-white font-bold rounded cursor-pointer transition duration-75 hover:bg-amber-500"
                     >Create</button>
                 </div> -->
-            </form>
         </div>
     </div>
 

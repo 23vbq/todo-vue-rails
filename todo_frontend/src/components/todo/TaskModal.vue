ae234c1f93269968c4094e1cf36eb520105e276b
23vbq <42277522+23vbq@users.noreply.github.com>
2025-04-06 16:11:54 +0200

    Revert "WIP fix datepicker"
    This reverts commit 96fff7fb633a26daeb785d23791f1324f8e8614d.

--- todo_frontend/src/components/todo/TaskModal.vue
+++ todo_frontend/src/components/todo/TaskModal.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onBeforeUnmount, onMounted } from 'vue';
+import { onMounted } from 'vue';
 
 import { Calendar } from 'vanilla-calendar-pro';
 import 'vanilla-calendar-pro/styles/index.css';
@@ -17,61 +17,28 @@ const props = defineProps({
 
 let taskDateCalendar;
 
-const getCardState = () => {
-    if (props.status == 1) {
-        return 'finished';
-    }
-    if (props.status == 0) {
-        const currentDate = new Date();
-        if (props.date < currentDate) {
-            return 'overdue';
-        }
-        return 'normal';
-    }
-}
-
-const cardState = getCardState();
-
 onMounted(() => {
-    const selectedDate = props.task.date;
-    const inputPos = document.getElementById('task-modal-date').getBoundingClientRect();
-
     taskDateCalendar = new Calendar('#task-modal-date', {
         inputMode: true,
         selectionTimeMode: 24,
-        positionToInput: 'center',
+        positionToInput: 'auto',
         styles: {
             calendar: 'vc z-30',
         },
         selectedTheme: 'light',
-        selectedMonth: selectedDate.getMonth(),
-        selectedYear: selectedDate.getFullYear(),
-        selectedDates: [selectedDate],
-        selectedTime: selectedDate.getHours() + ':' + selectedDate.getMinutes(),
-        onShow(self) {
+        onChangeToInput(self) {
             if (!self.context.inputElement) return;
-            self.context.mainElement.style.left = inputPos.x + 'px';
-            self.context.mainElement.style.top = inputPos.y + 'px';
-        },
-        onHide(self) {
-            if (!self.context.inputElement) {
-                console.error('Input element not found');
-                return;
-            }
-            if (self.context.selectedDates[0] && self.context.selectedTime) {
+            if (self.context.selectedDates[0]) {
                 self.context.inputElement.value = self.context.selectedDates[0] + ' ' + self.context.selectedTime;
+                self.hide();
             } else {
                 self.context.inputElement.value = '';
             }
             saveTask();
         },
     });
+    // taskDateCalendar.set();
     taskDateCalendar.init();
-});
-
-onBeforeUnmount(() => {
-    taskDateCalendar.hide();
-    taskDateCalendar.destroy();
 });
 
 const formatDate = (date) => {
@@ -85,7 +52,7 @@ const saveTask = () => {
 
     props.task.title = title;
     props.task.description = description;
-    props.task.date = new Date(date);
+    console.log(date);
 }
 </script>
 
